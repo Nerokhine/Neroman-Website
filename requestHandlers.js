31d9464fb05caebe5e2e8cc92f6530422a373e01
Nerokhine <lipton1@live.ca>
2017-01-15 15:31:25 -0500

    Initial commit

---
+++ requestHandlers.js
@@ -0,0 +1,20 @@
+
+function start(response) {
+	
+	var request=require('request');
+	request('http://google.com', function(err,res,body){
+	  response.writeHead(200, {"Content-Type": "text/html"});
+	  response.write(body);
+	  response.end();
+	  
+	});
+
+	console.log("Request handler 'start' was called.");
+}
+
+function upload(response) {
+	console.log("Request handler 'upload' was called.");
+}
+
+exports.start = start;
+exports.upload = upload;
