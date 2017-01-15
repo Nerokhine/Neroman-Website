f14775d97c7c3f403a43eb00bf0731459cd7cbc2
Nerokhine <lipton1@live.ca>
2017-01-15 16:25:47 -0500

    added index.html

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,13 +1,17 @@
-
 function start(response) {
 	
 	var request=require('request');
-	request('http://google.com', function(err,res,body){
+	/*request('http://google.com', function(err,res,body){
 	  response.writeHead(200, {"Content-Type": "text/html"});
 	  response.write(body);
 	  response.end();
 	  
-	});
+	});*/
+	var fs = require('fs');
+	var index = fs.readFileSync('index.html');
+	response.writeHead(200, {"Content-Type": "text/html"});
+	response.write(index);
+	response.end();
 
 	console.log("Request handler 'start' was called.");
 }
